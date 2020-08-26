73d2886a4879381f10b9ae853c5cb8d140dd30c5
Dennis Hahn <dennis@abstrahere.com>
2020-08-26 18:49:12 -0400

    arbol binario lua cheems

---
+++ arbolBinario.lua
@@ -0,0 +1,77 @@
+#!/usr/bin/lua5.3
+
+function creaNodo(numero) 
+    print("creando nodo")
+    local nuevoNodo = {
+        num = numero,
+        izq = nil,
+        der = nil
+    }
+    return nuevoNodo;
+end
+
+function imprimeNodo(nodo, depth)
+    if nodo ~= nil
+    then
+        local left = nodo.izq
+        local right = nodo.der
+        if left ~= nil
+        then
+            imprimeNodo(left, depth+1)
+        end
+        print(string.rep("--", depth)..nodo.num)
+        if right ~= nil
+        then
+            imprimeNodo(right, depth+1)
+        end
+    else
+        print("NIL")
+    end
+end
+
+function insertaElemento(nodo, numero)
+    print ("inserting", numero, "in", nodo)
+    local ret = nodo
+    if nodo == nil
+    then
+        ret = creaNodo(numero)
+    else
+        print("found", nodo.num)
+        if numero >= nodo.num
+        then
+            print("going right")
+            nodo.der = insertaElemento(nodo.der, numero)
+        else
+            print("going left")
+            nodo.izq = insertaElemento(nodo.izq, numero)
+        end
+    end
+    return ret
+end
+
+
+function cargarArbolBinario(arbol, arreglo)
+    --toma un arreglo de numeros y los inserta en arbol
+    --retorna el arbol con los numeros cargados
+end
+
+function encontrarNumero(arbol, numero)
+    -- retorna true si el numero esta en el arbol
+    -- retorna false si el numero no esta en el arbol
+end
+
+local arbol = nil
+
+arbol = insertaElemento(arbol, 5)
+arbol = insertaElemento(arbol, 1)
+arbol = insertaElemento(arbol, 11)
+arbol = insertaElemento(arbol, 4)
+arbol = insertaElemento(arbol, 11)
+arbol = insertaElemento(arbol, 0)
+arbol = insertaElemento(arbol, 9)
+arbol = insertaElemento(arbol, 20)
+arbol = insertaElemento(arbol, 21)
+
+imprimeNodo(arbol,1)
+
+-- print("arbol", arbol)
